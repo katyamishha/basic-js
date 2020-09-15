c4cb158674c9600a6ddf105d5598c29c09618d48
katyamishha <katyamishha@gmail.com>
2020-09-15 16:49:22 +0300

    feat: add decision simple-chain and recursive-depth

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(value === undefined){
+      this.arr.push('( )');
+    }else{
+      this.arr.push('( ' + String(value) + ' )');
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof (position) === 'number' && position + 1 > 0 && position > 0 && position <= this.arr.length) {
+      this.arr.splice(position - 1, 1);
+      return this;
+    } else {
+      this.arr = [];
+      throw new Error();
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join('~~');
+    this.arr = [];
+    return result;
   }
 };
 
