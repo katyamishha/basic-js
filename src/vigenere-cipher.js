b37769a517fef1780a1863c7d9d562f226f71a73
katyamishha <katyamishha@gmail.com>
2020-10-12 13:10:25 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(toReverse = true) {
+    this.toReverse = toReverse;
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = [],
+      count = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (count > key.length - 1) { 
+        count = 0;
+      }
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        result.push(message[i].charCodeAt());
+      } else {
+        result.push((message[i].charCodeAt() + key[count].charCodeAt()) % 26 + 65);
+        count++;
+      }
+    }
+
+    if (this.toReverse) {
+      return String.fromCharCode(...result);
+    } else {
+      return String.fromCharCode(...result.reverse());
+    };
+
+  }
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let result = [],
+        count = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (count > key.length - 1) { 
+        count = 0;
+      }
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        result.push(message[i].charCodeAt())
+      } else {
+        result.push((message[i].charCodeAt() - key[count].charCodeAt() + 26) % 26 + 65);
+        count++;
+      }
+    }
+
+    if (this.toReverse) {
+      return String.fromCharCode(...result);
+    } else {
+      return String.fromCharCode(...result.reverse());
+    };
   }
 }
 
